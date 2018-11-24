8235077f6639fc414bf5901bea2b79291b766739
Simon Samtleben <simon@samtleben.me>
2018-11-24 20:31:24 +0100

    Move where methods to separate classes
    In order to use where clauses in select, update and delete queries
    it seems to be a good idea to move these methods into separate classes.

--- src/Database/QueryBuilder/SelectQueryBuilder.php
+++ src/Database/QueryBuilder/SelectQueryBuilder.php
@@ -7,7 +7,7 @@ namespace Nekudo\ShinyCore\Database\QueryBuilder;
 /**
  * @property \Nekudo\ShinyCore\Database\StatementBuilder\SelectStatementBuilder $statementBuilder
  */
-class SelectQueryBuilder extends QueryBuilder
+class SelectQueryBuilder extends WhereQueryBuilder
 {
     /**
      * @var array $flags
@@ -28,11 +28,6 @@ class SelectQueryBuilder extends QueryBuilder
      * @var array $join
      */
     protected $join = [];
-
-    /**
-     * @var array $where
-     */
-    protected $where = [];
 
     /**
      * @var array $groupBy
@@ -139,176 +134,6 @@ class SelectQueryBuilder extends QueryBuilder
     }
 
     /**
-     * Adds a where condition.
-
-     * @param string $key
-     * @param string $operator
-     * @param mixed $value
-     * @return SelectQueryBuilder
-     */
-    public function where(string $key, string $operator, $value): SelectQueryBuilder
-    {
-        $this->addWhere($key, $operator, $value, 'AND');
-        return $this;
-    }
-
-    /**
-     * Adds "where equals" condition. (Shortcut for where method with '=' operator).
-     *
-     * @param string $key
-     * @param mixed $value
-     * @return SelectQueryBuilder
-     */
-    public function whereEquals(string $key, $value): SelectQueryBuilder
-    {
-        return $this->where($key, '=', $value);
-    }
-
-    /**
-     * Adds a "or where" condition.
-     *
-     * @param string $key
-     * @param string $operator
-     * @param mixed $value
-     * @return SelectQueryBuilder
-     */
-    public function orWhere(string $key, string $operator, $value): SelectQueryBuilder
-    {
-        $this->addWhere($key, $operator, $value, 'OR');
-        return $this;
-    }
-
-    /**
-     * Adds a "where in" condition.
-     *
-     * @param string $key
-     * @param array $values
-     * @return SelectQueryBuilder
-     */
-    public function whereIn(string $key, array $values): SelectQueryBuilder
-    {
-        $this->addWhere($key, 'IN', $values, 'AND');
-        return $this;
-    }
-
-    /**
-     * Adds a "where not in" condition.
-     *
-     * @param string $key
-     * @param array $values
-     * @return SelectQueryBuilder
-     */
-    public function whereNotIn(string $key, array $values): SelectQueryBuilder
-    {
-        $this->addWhere($key, 'NOT IN', $values, 'AND');
-        return $this;
-    }
-
-    /**
-     * Adds a "or where not in" condition.
-     *
-     * @param string $key
-     * @param array $values
-     * @return SelectQueryBuilder
-     */
-    public function orWhereIn(string $key, array $values): SelectQueryBuilder
-    {
-        $this->addWhere($key, 'IN', $values, 'OR');
-        return $this;
-    }
-
-    /**
-     * Adds a "or where not in" condition.
-     *
-     * @param string $key
-     * @param array $values
-     * @return SelectQueryBuilder
-     */
-    public function orWhereNotIn(string $key, array $values): SelectQueryBuilder
-    {
-        $this->addWhere($key, 'NOT IN', $values, 'OR');
-        return $this;
-    }
-
-    /**
-     * Adds a "where between" condition.
-     *
-     * @param string $key
-     * @param int $min
-     * @param int $max
-     * @return SelectQueryBuilder
-     */
-    public function whereBetween(string $key, int $min, int $max): SelectQueryBuilder
-    {
-        $values = ['min' => $min, 'max' => $max];
-        $this->addWhere($key, 'BETWEEN', $values, 'AND');
-        return$this;
-    }
-
-    /**
-     * Adds a "or where between" condition.
-     *
-     * @param string $key
-     * @param int $min
-     * @param int $max
-     * @return SelectQueryBuilder
-     */
-    public function orWhereBetween(string $key, int $min, int $max): SelectQueryBuilder
-    {
-        $values = ['min' => $min, 'max' => $max];
-        $this->addWhere($key, 'BETWEEN', $values, 'OR');
-        return$this;
-    }
-
-    /**
-     * Adds a "where null" condition.
-     *
-     * @param string $key
-     * @return SelectQueryBuilder
-     */
-    public function whereNull(string $key): SelectQueryBuilder
-    {
-        $this->addWhere($key, 'NULL', [], 'AND');
-        return $this;
-    }
-
-    /**
-     * Adds a "where not null" condition.
-     *
-     * @param string $key
-     * @return SelectQueryBuilder
-     */
-    public function whereNotNull(string $key): SelectQueryBuilder
-    {
-        $this->addWhere($key, 'NOT NULL', [], 'AND');
-        return $this;
-    }
-
-    /**
-     * Adds a "or where null" condition.
-     *
-     * @param string $key
-     * @return SelectQueryBuilder
-     */
-    public function orWhereNull(string $key): SelectQueryBuilder
-    {
-        $this->addWhere($key, 'NULL', [], 'OR');
-        return $this;
-    }
-
-    /**
-     * Adds a "or where not null" condition.
-     *
-     * @param string $key
-     * @return SelectQueryBuilder
-     */
-    public function orWhereNotNull(string $key): SelectQueryBuilder
-    {
-        $this->addWhere($key, 'NOT NULL', [], 'OR');
-        return $this;
-    }
-
-    /**
      * Adds a field to "group by".
      *
      * @param string $key
@@ -386,24 +211,6 @@ class SelectQueryBuilder extends QueryBuilder
     {
         $this->offset = $offset;
         return $this;
-    }
-
-    /**
-     * Adds where condition to pool.
-     *
-     * @param string $key
-     * @param string $operator
-     * @param $value
-     * @param string $concatenator
-     */
-    protected function addWhere(string $key, string $operator, $value, string $concatenator = 'AND'): void
-    {
-        array_push($this->where, [
-            'key' => $key,
-            'operator' => $operator,
-            'value' => $value,
-            'concatenator' => $concatenator,
-        ]);
     }
 
     /**
---
+++ src/Database/QueryBuilder/WhereQueryBuilder.php
@@ -0,0 +1,201 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nekudo\ShinyCore\Database\QueryBuilder;
+
+abstract class WhereQueryBuilder extends QueryBuilder
+{
+    /**
+     * @var array $where
+     */
+    protected $where = [];
+
+    /**
+     * Adds a where condition.
+     *
+     * @param string $key
+     * @param string $operator
+     * @param mixed $value
+     * @return WhereQueryBuilder
+     */
+    public function where(string $key, string $operator, $value): WhereQueryBuilder
+    {
+        $this->addWhere($key, $operator, $value, 'AND');
+        return $this;
+    }
+
+    /**
+     * Adds "where equals" condition. (Shortcut for where method with '=' operator).
+     *
+     * @param string $key
+     * @param mixed $value
+     * @return WhereQueryBuilder
+     */
+    public function whereEquals(string $key, $value): WhereQueryBuilder
+    {
+        return $this->where($key, '=', $value);
+    }
+
+    /**
+     * Adds a "or where" condition.
+     *
+     * @param string $key
+     * @param string $operator
+     * @param mixed $value
+     * @return WhereQueryBuilder
+     */
+    public function orWhere(string $key, string $operator, $value): WhereQueryBuilder
+    {
+        $this->addWhere($key, $operator, $value, 'OR');
+        return $this;
+    }
+
+    /**
+     * Adds a "where in" condition.
+     *
+     * @param string $key
+     * @param array $values
+     * @return WhereQueryBuilder
+     */
+    public function whereIn(string $key, array $values): WhereQueryBuilder
+    {
+        $this->addWhere($key, 'IN', $values, 'AND');
+        return $this;
+    }
+
+    /**
+     * Adds a "where not in" condition.
+     *
+     * @param string $key
+     * @param array $values
+     * @return WhereQueryBuilder
+     */
+    public function whereNotIn(string $key, array $values): WhereQueryBuilder
+    {
+        $this->addWhere($key, 'NOT IN', $values, 'AND');
+        return $this;
+    }
+
+    /**
+     * Adds a "or where not in" condition.
+     *
+     * @param string $key
+     * @param array $values
+     * @return WhereQueryBuilder
+     */
+    public function orWhereIn(string $key, array $values): WhereQueryBuilder
+    {
+        $this->addWhere($key, 'IN', $values, 'OR');
+        return $this;
+    }
+
+    /**
+     * Adds a "or where not in" condition.
+     *
+     * @param string $key
+     * @param array $values
+     * @return WhereQueryBuilder
+     */
+    public function orWhereNotIn(string $key, array $values): WhereQueryBuilder
+    {
+        $this->addWhere($key, 'NOT IN', $values, 'OR');
+        return $this;
+    }
+
+    /**
+     * Adds a "where between" condition.
+     *
+     * @param string $key
+     * @param int $min
+     * @param int $max
+     * @return WhereQueryBuilder
+     */
+    public function whereBetween(string $key, int $min, int $max): WhereQueryBuilder
+    {
+        $values = ['min' => $min, 'max' => $max];
+        $this->addWhere($key, 'BETWEEN', $values, 'AND');
+        return$this;
+    }
+
+    /**
+     * Adds a "or where between" condition.
+     *
+     * @param string $key
+     * @param int $min
+     * @param int $max
+     * @return WhereQueryBuilder
+     */
+    public function orWhereBetween(string $key, int $min, int $max): WhereQueryBuilder
+    {
+        $values = ['min' => $min, 'max' => $max];
+        $this->addWhere($key, 'BETWEEN', $values, 'OR');
+        return$this;
+    }
+
+    /**
+     * Adds a "where null" condition.
+     *
+     * @param string $key
+     * @return WhereQueryBuilder
+     */
+    public function whereNull(string $key): WhereQueryBuilder
+    {
+        $this->addWhere($key, 'NULL', [], 'AND');
+        return $this;
+    }
+
+    /**
+     * Adds a "where not null" condition.
+     *
+     * @param string $key
+     * @return WhereQueryBuilder
+     */
+    public function whereNotNull(string $key): WhereQueryBuilder
+    {
+        $this->addWhere($key, 'NOT NULL', [], 'AND');
+        return $this;
+    }
+
+    /**
+     * Adds a "or where null" condition.
+     *
+     * @param string $key
+     * @return WhereQueryBuilder
+     */
+    public function orWhereNull(string $key): WhereQueryBuilder
+    {
+        $this->addWhere($key, 'NULL', [], 'OR');
+        return $this;
+    }
+
+    /**
+     * Adds a "or where not null" condition.
+     *
+     * @param string $key
+     * @return WhereQueryBuilder
+     */
+    public function orWhereNotNull(string $key): WhereQueryBuilder
+    {
+        $this->addWhere($key, 'NOT NULL', [], 'OR');
+        return $this;
+    }
+
+    /**
+     * Adds where condition to pool.
+     *
+     * @param string $key
+     * @param string $operator
+     * @param $value
+     * @param string $concatenator
+     */
+    protected function addWhere(string $key, string $operator, $value, string $concatenator = 'AND'): void
+    {
+        array_push($this->where, [
+            'key' => $key,
+            'operator' => $operator,
+            'value' => $value,
+            'concatenator' => $concatenator,
+        ]);
+    }
+}
