a0595f2b406fc985769a8dc195ddecc42fc462cb
Simon Samtleben <simon@samtleben.me>
2018-11-30 22:30:46 +0100

    Add DeleteQueryBuilder test

---
+++ tests/Unit/Database/QueryBuilder/DeleteQueryBuilderTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Nekudo\ShinyCore\Tests\Unit\Database\QueryBuilder;
+
+use Nekudo\ShinyCore\Config;
+use Nekudo\ShinyCore\Database\Factory;
+use Nekudo\ShinyCore\Database\QueryBuilder\DeleteQueryBuilder;
+use Nekudo\ShinyCore\Tests\Unit\Database\DatabaseTest;
+
+class DeleteQueryBuilderTest extends DatabaseTest
+{
+    /**
+     * @var Config $config
+     */
+    public $config;
+
+    /**
+     * @var Factory $factory
+     */
+    public $factory;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $config = include __DIR__ . '/../../../Mocks/config.php';
+        $this->config = (new Config)->fromArray($config);
+        $this->factory = new Factory($this->config);
+    }
+
+    public function testFrom()
+    {
+        $queryBuilder = $this->factory->makeDelete();
+        $this->assertInstanceOf(DeleteQueryBuilder::class, $queryBuilder->from('customers'));
+    }
+
+    public function testDelete()
+    {
+        $queryBuilder = $this->factory->makeDelete();
+        $affectedRows = $queryBuilder->from('customers')
+            ->whereEquals('customer_id', 4)
+            ->delete();
+        $this->assertEquals(1, $affectedRows);
+    }
+}
