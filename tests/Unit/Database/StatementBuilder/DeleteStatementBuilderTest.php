617df0519432e03c8f9be44516c6380939dc6165
Simon Samtleben <simon@samtleben.me>
2018-12-03 20:18:31 +0100

    Add DeleteStatementBuilder test

---
+++ tests/Unit/Database/StatementBuilder/DeleteStatementBuilderTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Nekudo\ShinyCore\Tests\Unit\Database\StatementBuilder;
+
+use Nekudo\ShinyCore\Database\StatementBuilder\DeleteStatementBuilder;
+use PHPUnit\Framework\TestCase;
+
+class DeleteStatementBuilderTest extends TestCase
+{
+    public function testInitialization()
+    {
+        $builder = new DeleteStatementBuilder;
+        $this->assertEquals('DELETE', $builder->getStatement());
+    }
+
+    public function testAddFrom()
+    {
+        $builder = new DeleteStatementBuilder;
+        $builder->addFrom('customers');
+        $this->assertEquals("DELETE FROM `customers`" . PHP_EOL, $builder->getStatement());
+    }
+}
