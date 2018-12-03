47d2c6eb74b18243cc29855022087bfb905f7016
Simon Samtleben <simon@samtleben.me>
2018-12-03 20:28:38 +0100

    Add InsertStatementBuilder test

---
+++ tests/Unit/Database/StatementBuilder/InsertStatementBuilderTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Nekudo\ShinyCore\Tests\Unit\Database\StatementBuilder;
+
+use Nekudo\ShinyCore\Database\StatementBuilder\InsertStatementBuilder;
+use Nekudo\ShinyCore\Exception\Application\DatabaseException;
+use PHPUnit\Framework\TestCase;
+
+class InsertStatementBuilderTest extends TestCase
+{
+    public function testInitialization()
+    {
+        $builder = new InsertStatementBuilder;
+        $this->assertEquals('INSERT', $builder->getStatement());
+    }
+
+    public function testAddFlags()
+    {
+        $builder = new InsertStatementBuilder;
+        $builder->addFlags(['ignore' => true]);
+        $this->assertEquals('INSERT IGNORE', $builder->getStatement());
+    }
+
+    public function testAddInto()
+    {
+        $builder = new InsertStatementBuilder;
+        $builder->addInto('customers');
+        $this->assertEquals("INSERT INTO `customers`".PHP_EOL, $builder->getStatement());
+    }
+
+    public function testAddRows()
+    {
+        $builder = new InsertStatementBuilder;
+
+        // test valid rows:
+        $builder->addRows([['firstname' => 'Homer']]);
+        $this->assertEquals('INSERT (`firstname`) VALUES'.PHP_EOL.'(:firstname)', $builder->getStatement());
+
+        // test empty rows:
+        $this->expectException(DatabaseException::class);
+        $builder->addRows([]);
+    }
+}
