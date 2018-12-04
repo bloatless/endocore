71d078a2279318abf136b7ccec2a5a55b725e8d1
Simon Samtleben <simon@samtleben.me>
2018-12-04 21:41:29 +0100

    Add WhereStatementBuilder test

---
+++ tests/Mocks/WhereStatementBuilderMock.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Nekudo\ShinyCore\Tests\Mocks;
+
+use Nekudo\ShinyCore\Database\StatementBuilder\WhereStatementBuilder;
+
+class WhereStatementBuilderMock extends WhereStatementBuilder
+{
+
+}
