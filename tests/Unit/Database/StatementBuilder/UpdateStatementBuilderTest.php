9aa2cd89c2b280303d0198d99c3731dd64db5437
Simon Samtleben <simon@samtleben.me>
2018-12-04 21:20:03 +0100

    Add UpdateStatementBuilder test

---
+++ tests/Unit/Database/StatementBuilder/UpdateStatementBuilderTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Nekudo\ShinyCore\Tests\Unit\Database\StatementBuilder;
+
+use Nekudo\ShinyCore\Database\StatementBuilder\UpdateStatementBuilder;
+use PHPUnit\Framework\TestCase;
+
+class UpdateStatementBuilderTest extends TestCase
+{
+    public function testInitialization()
+    {
+        $builder = new UpdateStatementBuilder;
+        $this->assertEquals('UPDATE', $builder->getStatement());
+    }
+
+    public function testAddTable()
+    {
+        $builder = new UpdateStatementBuilder;
+        $builder->addTable('customers');
+        $this->assertEquals('UPDATE `customers`'. PHP_EOL, $builder->getStatement());
+    }
+
+    public function testAddCols()
+    {
+        $builder = new UpdateStatementBuilder;
+        $builder->addCols(['foo' => 'bar']);
+        $this->assertEquals('UPDATESET ' . PHP_EOL . '`foo` = :foo' . PHP_EOL, $builder->getStatement());
+    }
+}
